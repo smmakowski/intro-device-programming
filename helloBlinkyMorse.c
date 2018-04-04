47c65d01b842c2d43c95ec9f2ea791eca7f9450a
Stephen Makowski <makowski.sm@gmail.com>
2018-04-04 23:38:00 +0000

    implement morse code output helloBlinkyMorse

--- helloBlinkyMorse.c
+++ helloBlinkyMorse.c
@@ -9,15 +9,34 @@
 
 #include <wiringPi.h>
 #include <stdio.h>
-
-#define char MORSE[] = ".... . .-.. .-.. --- / .-- --- .-. .-.. -.."; // hello world in morse code
+#include <string.h>
 
 int main(void) {
   wiringPiSetup(); // init wiring pi standard pin definitons
   pinMode(0, OUTPUT); // inits GPIO pin 0 as output
-  
-  for (;;) {
 
+  char morse[] = ".... . .-.. .-.. ---/.-- --- .-. .-.. -..";
+  int i = 0;
+  // iterate through morse string
+  while (i < strlen(morse)) {
+    char blip = morse[i];
+    // check character and LED on
+    if (blip == '.') { // turns on light for one unit if .
+      digitalWrite(0, HIGH);
+      delay(100);
+      digitalWrite(0, LOW);
+    } else if (blip == '-') { // 3 units  on if -
+      digitalWrite(0, HIGH);
+      delay(300);
+      digitalWrite(0, LOW);
+    } else if (blip == ' ') { // 2 units for space between letters ( 1 less to accomodate auto delay between blips)
+      delay(200);
+    } else if (blip == '/') { // 6 units for word space (1 less than actual to accomadate atuo delay b/t blips)
+      delay(600);
+    }
+
+    delay(100); // space between parts of letters one unit
+    i++;
   }
 
   return 0; // end prgram (never reaches because endless loop)
