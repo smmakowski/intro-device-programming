e8f0ad0634c92936dd055055d1ea9e6d8c9c2edb
smmakowski <makowski.sm@gmail.com>
2018-04-03 20:18:55 -0700

    complete hello world

---
+++ helloWorld.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // preprocessor statement to load header file
+
+// main() is program entry point and must be present
+int main() { 
+  char invite[] = "Let\'s write some C";
+  printf("Hello, %s!", invite); // formatted printing 
+}
